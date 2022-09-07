d00c55c00776f776cc24a9d4b80378cef68f10e9
Cassie Kise <cassandrakise@gmail.com>
2022-09-06 22:21:37 -0700

    update for AskBCS

--- htmlRenderer.js
+++ htmlRenderer.js
@@ -11,7 +11,7 @@ function render(team) {
           
           <body>
             <section class="section">
-              <div class="container">
+              <div class="container">n
                 <div class="card">
                   <div class="card-content">
                     <div class="media-content">
