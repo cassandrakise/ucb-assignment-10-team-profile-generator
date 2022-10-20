878044e77f8d0b82236f4b48cdc8b8bea93134b5
Cassie Kise <cassandrakise@gmail.com>
2022-10-20 14:20:05 -0700

    updated htmlRenderer

--- htmlRenderer.js
+++ htmlRenderer.js
@@ -1,39 +1,33 @@
 function render(team) {
   return `
-      <!DOCTYPE html>
-        <html>
-            <head>
-              <meta charset="utf-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1">
-              <title>My Team</title>
-              <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-          </head>
-          
-          <body>
-            <section class="section">
-              <div class="container">n
-                <div class="card">
-                  <div class="card-content">
-                    <div class="media-content">
-                    <p class="bd-notification is-info"> ${renderCard(team)} </p>
-                    </div>
-                    <div>
-                    <p class="bd-notification is-info"> ${renderCard(team)} </p>
-                    </div>
-                    <div>
-                    <p class="bd-notification is-info"> ${renderCard(team)} </p>
-                    </div>
-                  </div>
-                </div>
+    <!DOCTYPE html>
+      <html>
+          <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>My Team</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+        </head>
+        
+        <body>
+          <section class="section">
+            <header class="hero is-primary"> 
+              <div class="hero-body title has-text-centered">
+                My Team 
               </div>
-                </p>
+            </header>
+            <div class="container">
+              <div class="columns is-centered is-multiline is-4">
+              ${renderCard(team)}
               </div>
-            </section>
-          </body>
-      </html>
-
-      
-    `;
+            </div>
+              </p>
+            </div>
+          </section>
+        </body>
+    </html> 
+    
+  `;
 }
 
 function renderCard(team) {
@@ -42,23 +36,36 @@ function renderCard(team) {
     let data = "";
     if (team[i].getRole() === "Manager") {
       data = `
-            <p> ${team[i].getOfficeNumber()} </p>
-            `;
+          <p> ${team[i].getOfficeNumber()} </p>
+          `;
     } else if (team[i].getRole() === "Engineer") {
       data = `
-            <p> ${team[i].getGitHub()} </p>
-            `;
+          <p> ${team[i].getGitHub()} </p>
+          `;
     } else if (team[i].getRole() === "Intern") {
       data = `
-            <p> ${team[i].getSchool()} </p>
-            `;
+          <p> ${team[i].getSchool()} </p>
+          `;
     }
-    let card = `<div>
-            <h1>${team[i].name}</h1>
-            <h2>${team[i].id}</h2>
-            <h2>${team[i].email}</h2>
-            ${data}
-        </div>`;
+    let card = `
+    <div class="column is-3">
+      <div class="card">
+        <header class="card-header has-background-danger-light">
+         <p class="card-header-title">
+          <h2 class="has-text-weight-bold">${team[i].name}</h2>
+          <h3>${team[i].getRole()}</h3>
+        </p>
+          <span class="card-header-icon">👤</span>
+        </header>
+      <div class="card-content">
+        <div class="media-content">
+          <h4>${team[i].id}</h4>
+          <h4>${team[i].email}</h4>
+          ${data}
+        </div>
+      </div>
+    </div>
+  </div>`;
 
     cards += card;
   }
